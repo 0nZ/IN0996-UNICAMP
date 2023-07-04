88e0ded08e02ccec05ef36c6ca1aea22856ade1d
Henrique Alves de Fernando <h236538@dac.unicamp.br>
2023-07-04 20:24:29 -0300

    definição dos eventos

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,58 @@ using System.Windows.Shapes;
 
 namespace IN0996_UNICAMP
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MediaPlayer : Window
+	{
+		public MediaPlayer()
+		{
+		}
+
+		private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+		}
+
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+		private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+		}
+
+		private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+		private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+		}
+
+		private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+		private void Next_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+		}
+
+		private void Next_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+        private void Previous_CanExecute(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+        private void Previous_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+        private void Repeat_CanExecute(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+
+        private void Repeat_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+		}
+	}
 }
